ec32b65d0ebe182330e88597351c1f4860929e0f
Tenfleques <tendaichikake@gmail.com>
2018-12-24 09:24:05 +0300

    Delete sync.sh

--- sync.sh
+++
@@ -1,2 +0,0 @@
-
-rsync -rRq --exclude sync.sh --exclude Dockerfile --exclude run.sh --exclude .git --exclude README.md --exclude LICENSE  --exclude .gitignore --exclude docker-compose.yaml --exclude configs ./ tendai@flequesboard.com:~/task.book.ilearnfb.com/
